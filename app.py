ae096ffd96e021466cd6d1f62c142e7ca5c6a603
schollar <cschollar22@gmail.com>
2022-01-29 16:42:38 -0700

    Basic blog post functionality done, working on login system

--- app.py
+++ app.py
@@ -2,20 +2,26 @@ import dbcreds
 import dbhandler as dbh
 
 print('Welcome to the Blog Site')
-username = input("Enter a username: ")
-print('Welcome', username, ' please select an option: ')
+print('Please login to continue')
+dbh.dbInteraction.user_login()
+print('Welcome', {dbh.dbInteraction.username},
+      ' please select an option: ')
 
-while True:
-    try:
-        print('1. Write a new post')
-        print('2. See all posts')
-        selection = input("Select an option: ")
-        if(float(selection) == 1):
-            dbh.dbInteraction.write_post(username)
-        elif(float(selection) == 2):
-            dbh.dbInteraction.show_posts()
-        else:
-            print('You must select a valid option')
-    except KeyboardInterrupt:
-        print('You have quit the app!')
-        exit()
+# while True:
+#     try:
+#         print('1. Write a new post')
+#         print('2. See all posts')
+#         print('Type "exit" to exit the app')
+#         selection = input("Select an option: ")
+#         if(selection == 'exit'):
+#             print('Thank you come again!')
+#             exit()
+#         elif(float(selection) == 1):
+#             dbh.dbInteraction.write_post(username)
+#         elif(float(selection) == 2):
+#             dbh.dbInteraction.show_posts()
+#         else:
+#             print('You must select a valid option')
+#     except KeyboardInterrupt:
+#         print('You have quit the app!')
+#         exit()
--- dbhandler.py
+++ dbhandler.py
@@ -29,3 +29,24 @@ class dbInteraction:
         conn.close()
         for post in posts:
             print(post[0], ':', post[1])
+
+    def user_login():
+        username = input("Enter a username: ")
+        password = input("Enter your password: ")
+        conn = db.connect(user=dbcreds.user, password=dbcreds.password,
+                          host=dbcreds.host, port=dbcreds.port, database=dbcreds.database)
+        cursor = conn.cursor()
+
+        cursor.execute(
+            f"SELECT * FROM users WHERE username = '{username}' and password = '{password}'")
+        user = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        if(user == []):
+            print("Invalid username or password!")
+        else:
+            for i in user:
+                print(f'Welcome ', i[1])
+
+
+username = 'foo'
