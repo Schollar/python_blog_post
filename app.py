a471b25bf0fc66dd519a6f02cc4057cbf143737f
schollar <cschollar22@gmail.com>
2022-02-01 14:32:24 -0700

    Created two new functions to make code more DRY, added colorama to make program a bit nicer on the eyes

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import dbhandler as dbh
-
+from colorama import init, Fore, Back, Style
 # Printing welcome message, taking in username and password inputs from user and saving to a variable
 print('Welcome to the Blog Site')
 print('Please login to continue')
 username = input("Enter a username: ")
 password = input("Enter your password: ")
+db = dbh.dbInteraction()
+# Initilize colorama to make our program pretty!
+init(autoreset=True)
 # If our user login function returns false, we exit the program, else the user has logged in and we run the loop.
-if(dbh.dbInteraction.user_login(username, password) == False):
+if(db.user_login(username, password) == False):
     exit()
 else:
     # Infinite loop that runs once user is logged in, user can make a selection and based on user selection a function gets
@@ -16,17 +19,17 @@ else:
     while(True):
         try:
             print('Please select an option: ')
-            print('1. Write a new post')
-            print('2. See all posts')
-            print('Type "exit" to exit the app')
+            print(Fore.RED + '1. Write a new post')
+            print(Fore.GREEN + '2. See all posts')
+            print('Type', Fore.RED + "exit", 'to exit the app')
             selection = input("Select an option: ")
             if(selection == 'exit'):
                 print('Thank you come again!')
                 exit()
             elif(float(selection) == 1):
-                dbh.dbInteraction.write_post(username)
+                db.write_post(username)
             elif(float(selection) == 2):
-                dbh.dbInteraction.show_posts()
+                db.show_posts()
             else:
                 print('You must select a valid option')
         except KeyboardInterrupt:
--- dbhandler.py
+++ dbhandler.py
@@ -3,55 +3,55 @@ import mariadb as db
 
 
 class dbInteraction:
+
+    # Made two new functions, one that creates a conn to db and creates a cursor and returns them. The other closes the conn and cursor
+    def db_connect(self):
+        conn = db.connect(user=dbcreds.user, password=dbcreds.password,
+                          host=dbcreds.host, port=dbcreds.port, database=dbcreds.database)
+        cursor = conn.cursor()
+        return conn, cursor
+
+    def db_disconnect(self, conn, cursor):
+        cursor.close()
+        conn.close()
     # Function That takes in a user post input saves it to a variable
     # Then we make a connection to the database, create a cursor
     # Execute an insert SQL query to insert the post content and user's username into the DB. Username was passed to function as argument
     # Commit our changes to the DB, close the cursor and then close the connection
-    def write_post(username):
+
+    def write_post(self, username):
         content = input('Write your post:')
-        conn = db.connect(user=dbcreds.user, password=dbcreds.password,
-                          host=dbcreds.host, port=dbcreds.port, database=dbcreds.database)
-        cursor = conn.cursor()
+        conn, cursor = self.db_connect()
         cursor.execute(
             f"select id from users where username = '{username}'")
         user = cursor.fetchone()
         cursor.execute(
             f"INSERT INTO python_blog.blog_post(content, user_id) VALUES('{content}', '{user[0]}')")
         conn.commit()
-        cursor.close()
-        conn.close()
+        self.db_disconnect(conn, cursor)
 # Function that creates DB connection, creates cursor, runs a select SQL query to select all blog posts and
 # usernames attached to post from our DB, fetch all the posts, close our cursor and DB connection, and then
 # loop through the posts we saved to a variable from the SQL query and display the username and content using print
 
-    def show_posts():
-        conn = db.connect(user=dbcreds.user, password=dbcreds.password,
-                          host=dbcreds.host, port=dbcreds.port, database=dbcreds.database)
-        cursor = conn.cursor()
-
+    def show_posts(self):
+        conn, cursor = self.db_connect()
         cursor.execute(
             f"select content, users.username  from blog_post inner join users on users.id = blog_post.user_id")
         posts = cursor.fetchall()
-
-        cursor.close()
-        conn.close()
+        self.db_disconnect(conn, cursor)
         for post in posts:
-            print(post[1], ':', post[0])
+            print(post[1], ' : ', post[0])
 # Function that takes in the username and password, creates connection to DB, creates cursor and runs a
 # select query to grab username and password where the username and password matches that of the arguments given
 # Check to see if our user variable is empty from select query, if it is we return false since no username and password from the DB matched
 # If user varable is not empty we got a match from the DB and print a welcome message
 
-    def user_login(username, password):
-        conn = db.connect(user=dbcreds.user, password=dbcreds.password,
-                          host=dbcreds.host, port=dbcreds.port, database=dbcreds.database)
-        cursor = conn.cursor()
-
+    def user_login(self, username, password):
+        conn, cursor = self.db_connect()
         cursor.execute(
             f"SELECT * FROM users WHERE username = '{username}' and password = '{password}'")
         user = cursor.fetchone()
-        cursor.close()
-        conn.close()
+        self.db_disconnect(conn, cursor)
         if(user == None):
             print("Invalid username or password!")
             return False
